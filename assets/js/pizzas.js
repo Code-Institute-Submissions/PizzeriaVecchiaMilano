93506bee70aabd99a9b8f463407fe8ddc5baf6ae
OmarBedawi <omarbedawi@hotmail.com>
2020-11-06 12:46:18 +0000

    pizza foto added and pizza.js completed. menu page update with all the buttons

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,9 +1,54 @@
 $(document).ready(function(){
-             $('#margherita').hover(function(){
-                 $('assets/images/pizzemenu/margherita.jpg').show(2000);
-         })
-         });
+$('#show-image1').on("click", function(){
+  $('#button1').toggle('slow');
+  });
+});
 
-$('#margherita').hover(function(){
-    this.src = "assets/images/pizzemenu/margherita.jpg";
-})  
+
+$(document).ready(function(){
+$('#show-image2').on("click", function(){
+  $('#button2').toggle('slow');
+  });
+});
+
+
+$(document).ready(function(){
+$('#show-image3').on("click", function(){
+  $('#button3').toggle('slow');
+  });
+});
+
+
+$(document).ready(function(){
+$('#show-image4').on("click", function(){
+  $('#button4').toggle('slow');
+  });
+});
+
+
+$(document).ready(function(){
+$('#show-image5').on("click", function(){
+  $('#button5').toggle('slow');
+  });
+});
+
+
+$(document).ready(function(){
+$('#show-image6').on("click", function(){
+  $('#button6').toggle('slow');
+  });
+});
+
+
+$(document).ready(function(){
+$('#show-image7').on("click", function(){
+  $('#button7').toggle('slow');
+  });
+});
+
+
+$(document).ready(function(){
+$('#show-image8').on("click", function(){
+  $('#button8').toggle('slow');
+  });
+});
