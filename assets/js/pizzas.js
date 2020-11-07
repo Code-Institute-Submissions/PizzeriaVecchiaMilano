8509f5b9f9d636525544c5d28167bcac03bf3d72
OmarBedawi <omarbedawi@hotmail.com>
2020-11-07 18:22:36 +0000

    file duplicated deleted and all html files reviewed before the final validation

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,9 +1,27 @@
+////////////////////////////////////////////////////////////////////////BUTTON 1
 $(document).ready(function(){
 $('#show-image1').on("click", function(){
   $('#button1').toggle('slow');
   });
 });
 
+
+$("#show-image1").click(function() {
+  
+   var lable = $("#show-image1").text().trim();
+
+   if(lable == "Show") {
+     $("#show-image1").text("Hide");
+     $("#button1").hide();
+   }
+   else {
+     $("#show-image1").text("Show");
+     $("#button1").show();
+   }
+    
+  });
+
+////////////////////////////////////////////////////////////////////////BUTTON 2
 
 $(document).ready(function(){
 $('#show-image2').on("click", function(){
@@ -12,6 +30,22 @@ $('#show-image2').on("click", function(){
 });
 
 
+$("#show-image2").click(function() {
+  
+   var lable = $("#show-image2").text().trim();
+
+   if(lable == "Show") {
+     $("#show-image2").text("Hide");
+     $("#button2").hide();
+   }
+   else {
+     $("#show-image2").text("Show");
+     $("#button2").show();
+   }
+    
+  });
+////////////////////////////////////////////////////////////////////////BUTTON 3
+
 $(document).ready(function(){
 $('#show-image3').on("click", function(){
   $('#button3').toggle('slow');
@@ -19,6 +53,22 @@ $('#show-image3').on("click", function(){
 });
 
 
+$("#show-image3").click(function() {
+  
+   var lable = $("#show-image3").text().trim();
+
+   if(lable == "Show") {
+     $("#show-image3").text("Hide");
+     $("#button3").hide();
+   }
+   else {
+     $("#show-image3").text("Show");
+     $("#button3").show();
+   }
+    
+  });
+
+////////////////////////////////////////////////////////////////////////BUTTON 4
 $(document).ready(function(){
 $('#show-image4').on("click", function(){
   $('#button4').toggle('slow');
@@ -26,13 +76,44 @@ $('#show-image4').on("click", function(){
 });
 
 
+$("#show-image4").click(function() {
+  
+   var lable = $("#show-image4").text().trim();
+
+   if(lable == "Show") {
+     $("#show-image4").text("Hide");
+     $("#button4").hide();
+   }
+   else {
+     $("#show-image4").text("Show");
+     $("#button4").show();
+   }
+    
+  });
+
+////////////////////////////////////////////////////////////////////////BUTTON 5
 $(document).ready(function(){
 $('#show-image5').on("click", function(){
   $('#button5').toggle('slow');
   });
 });
 
+$("#show-image5").click(function() {
+  
+   var lable = $("#show-image5").text().trim();
 
+   if(lable == "Show") {
+     $("#show-image5").text("Hide");
+     $("#button5").hide();
+   }
+   else {
+     $("#show-image5").text("Show");
+     $("#button5").show();
+   }
+    
+  });
+
+////////////////////////////////////////////////////////////////////////BUTTON 6
 $(document).ready(function(){
 $('#show-image6').on("click", function(){
   $('#button6').toggle('slow');
@@ -40,6 +121,22 @@ $('#show-image6').on("click", function(){
 });
 
 
+$("#show-image6").click(function() {
+  
+   var lable = $("#show-image6").text().trim();
+
+   if(lable == "Show") {
+     $("#show-image6").text("Hide");
+     $("#button6").hide();
+   }
+   else {
+     $("#show-image6").text("Show");
+     $("#button6").show();
+   }
+    
+  });
+
+////////////////////////////////////////////////////////////////////////BUTTON 7
 $(document).ready(function(){
 $('#show-image7').on("click", function(){
   $('#button7').toggle('slow');
@@ -47,8 +144,40 @@ $('#show-image7').on("click", function(){
 });
 
 
+$("#show-image7").click(function() {
+  
+   var lable = $("#show-image7").text().trim();
+
+   if(lable == "Show") {
+     $("#show-image7").text("Hide");
+     $("#button7").hide();
+   }
+   else {
+     $("#show-image7").text("Show");
+     $("#button7").show();
+   }
+    
+  });
+
+////////////////////////////////////////////////////////////////////////BUTTON 8
 $(document).ready(function(){
 $('#show-image8').on("click", function(){
   $('#button8').toggle('slow');
   });
 });
+
+
+$("#show-image8").click(function() {
+  
+   var lable = $("#show-image8").text().trim();
+
+   if(lable == "Show") {
+     $("#show-image8").text("Hide");
+     $("#button8").hide();
+   }
+   else {
+     $("#show-image8").text("Show");
+     $("#button8").show();
+   }
+    
+  });
