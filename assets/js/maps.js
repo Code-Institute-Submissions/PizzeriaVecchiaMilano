a3dbf88e464c179105aa0748f0025d62328ed4a5
OmarBedawi <omarbedawi@hotmail.com>
2020-11-10 17:33:54 +0000

    all the html, css and js files went for the final validation and formatting. Some captions added for the readme file

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,25 +1,29 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
-        center: {
-            lat: 45.464211,
-            lng: 9.191383 
-        }
-    });
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 12,
+		center: {
+			lat: 45.464211,
+			lng: 9.191383
+		}
+	});
 
-    var labels = "";
+	var labels = "";
 
-    var locations = [
-        { lat: 45.464211, lng: 9.191383 },
-        
-    ];
+	var locations = [{
+			lat: 45.464211,
+			lng: 9.191383
+		},
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
+	];
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+	var markers = locations.map(function (location, i) {
+		return new google.maps.Marker({
+			position: location,
+			label: labels[i % labels.length]
+		});
+	});
+
+	var markerCluster = new MarkerClusterer(map, markers, {
+		imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+	});
 }
--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,183 +1,181 @@
 ////////////////////////////////////////////////////////////////////////BUTTON 1
-$(document).ready(function(){
-$('#show-image1').on("click", function(){
-  $('#button1').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image1').on("click", function () {
+		$('#button1').toggle('slow');
+	});
 });
 
+//Function to alternate the text Show or Hide on the button
+$("#show-image1").click(function () {
 
-$("#show-image1").click(function() {
-  
-   var lable = $("#show-image1").text().trim();
+	var lable = $("#show-image1").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image1").text("Hide");
-     $("#button1").hide();
-   }
-   else {
-     $("#show-image1").text("Show");
-     $("#button1").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image1").text("Hide");
+		$("#button1").hide();
+	} else {
+		$("#show-image1").text("Show");
+		$("#button1").show();
+	}
+
+});
 
 ////////////////////////////////////////////////////////////////////////BUTTON 2
-
-$(document).ready(function(){
-$('#show-image2').on("click", function(){
-  $('#button2').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image2').on("click", function () {
+		$('#button2').toggle('slow');
+	});
 });
 
+//Function to alternate the text Show or Hide on the button
+$("#show-image2").click(function () {
 
-$("#show-image2").click(function() {
-  
-   var lable = $("#show-image2").text().trim();
+	var lable = $("#show-image2").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image2").text("Hide");
-     $("#button2").hide();
-   }
-   else {
-     $("#show-image2").text("Show");
-     $("#button2").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image2").text("Hide");
+		$("#button2").hide();
+	} else {
+		$("#show-image2").text("Show");
+		$("#button2").show();
+	}
+
+});
 ////////////////////////////////////////////////////////////////////////BUTTON 3
-
-$(document).ready(function(){
-$('#show-image3').on("click", function(){
-  $('#button3').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image3').on("click", function () {
+		$('#button3').toggle('slow');
+	});
 });
 
+//Function to alternate the text Show or Hide on the button
+$("#show-image3").click(function () {
 
-$("#show-image3").click(function() {
-  
-   var lable = $("#show-image3").text().trim();
+	var lable = $("#show-image3").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image3").text("Hide");
-     $("#button3").hide();
-   }
-   else {
-     $("#show-image3").text("Show");
-     $("#button3").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image3").text("Hide");
+		$("#button3").hide();
+	} else {
+		$("#show-image3").text("Show");
+		$("#button3").show();
+	}
+
+});
 
 ////////////////////////////////////////////////////////////////////////BUTTON 4
-$(document).ready(function(){
-$('#show-image4').on("click", function(){
-  $('#button4').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image4').on("click", function () {
+		$('#button4').toggle('slow');
+	});
 });
 
+//Function to alternate the text Show or Hide on the button
+$("#show-image4").click(function () {
 
-$("#show-image4").click(function() {
-  
-   var lable = $("#show-image4").text().trim();
+	var lable = $("#show-image4").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image4").text("Hide");
-     $("#button4").hide();
-   }
-   else {
-     $("#show-image4").text("Show");
-     $("#button4").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image4").text("Hide");
+		$("#button4").hide();
+	} else {
+		$("#show-image4").text("Show");
+		$("#button4").show();
+	}
+
+});
 
 ////////////////////////////////////////////////////////////////////////BUTTON 5
-$(document).ready(function(){
-$('#show-image5').on("click", function(){
-  $('#button5').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image5').on("click", function () {
+		$('#button5').toggle('slow');
+	});
 });
+//Function to alternate the text Show or Hide on the button
+$("#show-image5").click(function () {
 
-$("#show-image5").click(function() {
-  
-   var lable = $("#show-image5").text().trim();
+	var lable = $("#show-image5").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image5").text("Hide");
-     $("#button5").hide();
-   }
-   else {
-     $("#show-image5").text("Show");
-     $("#button5").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image5").text("Hide");
+		$("#button5").hide();
+	} else {
+		$("#show-image5").text("Show");
+		$("#button5").show();
+	}
+
+});
 
 ////////////////////////////////////////////////////////////////////////BUTTON 6
-$(document).ready(function(){
-$('#show-image6').on("click", function(){
-  $('#button6').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image6').on("click", function () {
+		$('#button6').toggle('slow');
+	});
 });
 
+//Function to alternate the text Show or Hide on the button
+$("#show-image6").click(function () {
 
-$("#show-image6").click(function() {
-  
-   var lable = $("#show-image6").text().trim();
+	var lable = $("#show-image6").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image6").text("Hide");
-     $("#button6").hide();
-   }
-   else {
-     $("#show-image6").text("Show");
-     $("#button6").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image6").text("Hide");
+		$("#button6").hide();
+	} else {
+		$("#show-image6").text("Show");
+		$("#button6").show();
+	}
+
+});
 
 ////////////////////////////////////////////////////////////////////////BUTTON 7
-$(document).ready(function(){
-$('#show-image7').on("click", function(){
-  $('#button7').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image7').on("click", function () {
+		$('#button7').toggle('slow');
+	});
 });
 
+//Function to alternate the text Show or Hide on the button
+$("#show-image7").click(function () {
 
-$("#show-image7").click(function() {
-  
-   var lable = $("#show-image7").text().trim();
+	var lable = $("#show-image7").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image7").text("Hide");
-     $("#button7").hide();
-   }
-   else {
-     $("#show-image7").text("Show");
-     $("#button7").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image7").text("Hide");
+		$("#button7").hide();
+	} else {
+		$("#show-image7").text("Show");
+		$("#button7").show();
+	}
+
+});
 
 ////////////////////////////////////////////////////////////////////////BUTTON 8
-$(document).ready(function(){
-$('#show-image8').on("click", function(){
-  $('#button8').toggle('slow');
-  });
+//Function to make picture appears
+$(document).ready(function () {
+	$('#show-image8').on("click", function () {
+		$('#button8').toggle('slow');
+	});
 });
 
+//Function to alternate the text Show or Hide on the button 
+$("#show-image8").click(function () {
 
-$("#show-image8").click(function() {
-  
-   var lable = $("#show-image8").text().trim();
+	var lable = $("#show-image8").text().trim();
 
-   if(lable == "Show") {
-     $("#show-image8").text("Hide");
-     $("#button8").hide();
-   }
-   else {
-     $("#show-image8").text("Show");
-     $("#button8").show();
-   }
-    
-  });
+	if (lable == "Show") {
+		$("#show-image8").text("Hide");
+		$("#button8").hide();
+	} else {
+		$("#show-image8").text("Show");
+		$("#button8").show();
+	}
+
+});
